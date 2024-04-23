a915d68db2bc92aeef0fd6b79e6299ea1a18a34a
Asus <mkmani176@gmail.com>
2024-04-23 18:57:25 +0530

    finish

--- JS/cart.js
+++ JS/cart.js
@@ -1,12 +1,16 @@
 $(document).ready(() => {
-  const view = () => {
-    $.ajax({
-      url: "https://dummyjson.com/products",
-      success: (e) => {
-        let data = e.products;
-        console.log(data);
-        let rend = data.map((ele) => {
-          return ` <div class="cards" id=${ele.id} >
+  //check the login session
+  let login = sessionStorage.getItem("login");
+  let session = JSON.parse(login);
+  if (session == true) {
+    const view = () => {
+      $.ajax({
+        url: "https://dummyjson.com/products",
+        success: (e) => {
+          let data = e.products;
+          console.log(data);
+          let rend = data.map((ele) => {
+            return ` <div class="cards" id=${ele.id} >
                 <div class="card-image">  <img src="${ele.images[0]}" alt="test" ></div>
                   
                   
@@ -18,20 +22,22 @@ $(document).ready(() => {
                   </span>
                   <button id="buy">Buy now</button>
               </div>`;
-        });
-        $(".card").append(rend);
-      },
-    });
-  };
-  view();
+          });
+          $(".card").append(rend);
+        },
+      });
+    };
+    view();
 
-  //add data
-  $("#add").click((e) => {
-    e.preventDefault();
-    $(".add h2").show();
+    //add data
+    $("#add").click((e) => {
+      e.preventDefault();
+      $(".add h2").show();
 
-    $(".card").hide();
-    $(".add").append(`
+      //update hide
+      $(".update").hide();
+      $(".card").hide();
+      $(".add").append(`
             <div class="detail-form">
             <form >
                 <input type="text" placeholder="title" id="title">
@@ -42,98 +48,104 @@ $(document).ready(() => {
         </div>
         `);
 
-    let title;
-    let price;
-    let files;
+      let title;
+      let price;
+      let files;
 
-    $(".addSub").click((e) => {
-      e.preventDefault();
-      $(".add form").hide();
-      title = $("#title").val();
-      price = $("#price").val();
-      files = $("#files").val();
+      $(".addSub").click((e) => {
+        e.preventDefault();
+        $(".add form").hide();
+        title = $("#title").val();
+        price = $("#price").val();
+        files = $("#files").val();
 
-      let obj = {
-        title: title,
-        price: price,
-        images: [files],
-      };
+        let obj = {
+          title: title,
+          price: price,
+          images: [files],
+        };
 
-      $.ajax({
-        url: "https://dummyjson.com/product/add",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(obj),
-        success: (pro) => {
-          console.log(pro);
-          let res = ` <div class="cards">
+        $.ajax({
+          url: "https://dummyjson.com/product/add",
+          type: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(obj),
+          success: (pro) => {
+            console.log(pro);
+            let res = ` <div class="cards">
                 <div class="card-image">  <img src="${pro.images}" alt="test" ></div>
                   <h2>${pro.title}</h2>
                   <p>Rs.${pro.price}</p>
                   <button>Buy now</button>
               </div>`;
 
-          $(".new").append(res);
-        },
+            $(".new").append(res);
+          },
+        });
       });
     });
-  });
 
-  //update
-  $(".card").on("click", ".cards i:first-child", (event) => {
-    $(".card").hide();
+    //update
+    $(".card").on("click", ".cards i:first-child", (event) => {
+      //when iam click the edit icon the card container will display none
+      $(".card").hide();
 
-    let ele = $(event.currentTarget.parentElement.parentElement);
+      let ele = $(event.currentTarget).closest(".cards")
+      
+      let image = ele.find(" img").attr("src");
+      let title = ele.find(" h2").text();
+      let price = ele.find(" p").text();
 
-    let image = ele.find(" img").attr("src");
-    let title = ele.find(" h2").text();
-    let price = ele.find(" p").text();
+      let form = `<div class="detail-form">
+        <form >
+        <input type="text" placeholder="title" id="title" value="${title}">
+        <input type="text" placeholder="price" id="price" value=$${price}>
+        <input type="text" name="" id="files" placeholder="img url" value=${image}>
+        <button id="updatesub">Update</button>
+    </form>
+    </div>`;
 
-    
-    let form = `<div class="detail-form">
-      <form >
-      <input type="text" placeholder="title" id="title" value="${title}">
-      <input type="text" placeholder="price" id="price" value=$${price}>
-      <input type="text" name="" id="files" placeholder="img url" value=${image}>
-      <button id="updatesub">Update</button>
-  </form>
-  </div>`;
+      $(".update").append(form);
 
-    $(".update").append(form);
+      $("#updatesub").click((E) => {
+        E.preventDefault();
+        //update title show
+        $(".update h2").show();
 
-    $("#updatesub").click((E) => {
-      E.preventDefault();
-      $(".update h2").show();
+        let title = $("#title").val();
+        let price = $("#price").val();
+        let image = $("#files").val();
 
-      let title = $("#title").val();
-      let price = $("#price").val();
-      let image = $("#files").val();
-
-      let upda = `<div class="cards">
+        let upda = `<div class="cards">
     <div class="card-image">  <img src="${image}" alt="test" ></div>
       <h2>${title}</h2>
       <p>Rs.${price}</p>
       <button>Buy now</button>
   </div>`;
-      $(".update-show").append(upda);
-      $(".update form").hide();
+        $(".update-show").append(upda);
+        $(".update form").hide();
+      });
     });
-  });
 
-  //delete
+    //delete
 
-  $(document).on("click","i:last-child",(event)=>{
-      let ele=$(event.currentTarget.parentElement.parentElement.parentElement)
-      
-      let id=ele.find(".cards").attr("id")
-      console.log(id);
+    $(document).on("click", ".cards i:last-child", (event) => {
+      //geting elements
+      let ele = $(event.currentTarget).closest(".cards");
+      //elements id
+      let id = ele.attr("id");
+      // console.log(id);
       $.ajax({
-        url:`https://dummyjson.com/products/${id}`, 
-        method:"DELETE",
-        success:(del)=>{
-           alert("Deleted successfully")
-           location.reload()
-          }
-      })
-  })
+        url: `https://dummyjson.com/products/${id}`,
+        method: "DELETE",
+        success: (del) => {
+          console.log(del);
+          alert("Deleted successfully");
+          location.reload();
+        },
+      });
+    });
+  } else {
+    location.assign("./index.html");
+  }
 });
--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
 $(document).ready(() => {
+  //session handling
+  sessionStorage.clear()
+  let login=false;
+  //hide the two error message elements
   $(".err-user").hide();
   $(".err-pwd").hide();
+
+  ////form submit button
   $("#submit").click((e) => {
     e.preventDefault();
     let username = $("#user").val();
@@ -27,82 +33,64 @@ $(document).ready(() => {
       });
     }
 
+    
+
     //authentication
-    // let uname;
-    // let pass;
-
-    // let cred;
-    // //ajax
-    // $.ajax({
-    //    url:"https://dummyjson.com/user/",
-    //   success: (e) => {
-    //     cred = e.users;
-    //     // console.log(cred)
-    //     let id;
-        
-    //     id = cred.filter((e) => {
-    //       return e.id == 3;
-    //     });
-    //     id.map((ele) => {
-    //       uname = ele.username;
-    //       pass = ele.password;
-    //     });
-    //   },
-    // }).done(() => {
-
-    //     //username and password authentication
-    //   try {
-    //     if (username == uname && password == pass) {
-    //       document.location.assign("./view.html")
-    //     } else {
-    //     alert("wrong");
-    //     }
-    //   } catch (e) {
-    //     document.write(e);
-    //   }
-    // });
     let uname;
     let pass;
-  
-    let cred=localStorage.getItem("cred")
-  
-  let data=JSON.parse(cred);
-    
-    uname=data.user
-    pass=data.password
-  
-    //username and password authentication
-    try{
-      if
-      (uname==username && pass==password){
-        location.assign("./view.html")
+
+    let cred;
+    //ajax
+    $.ajax({
+      url: "https://dummyjson.com/user/",
+      success: (e) => {
+        cred = e.users;
+       
+        let id;
+
+        id = cred.filter((e) => {
+          return e.id == 3;
+        });
+        id.map((ele) => {
+          uname = ele.username;
+          pass = ele.password;
+        });
+      },
+    }).done(() => {
+      //username and password authentication
+      try {
+        if (username == uname && password == pass) {
+          login=true
+          sessionStorage.setItem("login",JSON.stringify(login))
+          if(login==true){
+            document.location.assign("./view.html");
+          }
+          else{
+            alert("Session expired")
+          }
+        } else {
+          alert("Please enter the details");
+          location.reload()
+        }
+      } catch (e) {
+        document.write(e);
       }
-      else{
-        alert("incorrect details")
-      }
-    }catch(ele){
-      document.write(ele)
-    }
-   
+    });
   });
 
-  let clicked=false
-  $("#icon").click(()=>{
-    clicked=!clicked
 
-    if(clicked)
-    {
-      $("#pwd").attr("type","text")
-      $("#icon").attr("class","fa-solid fa-eye")
+  //eye icon
+  let clicked = false;
+  $("#icon").click(() => {
+    console.log("test");
+    clicked = !clicked;
+    console.log("test");
+    if (clicked) {
+      $("#pwd").attr("type", "text");
+      $("#icon").attr("class", "fa-solid fa-eye");
+    } else if ($("#icon").attr("class", "fa-solid fa-eye")) {
+      $("#pwd").attr("type", "password");
+      $("#icon").attr("class", "fa-solid fa-eye-slash");
     }
-    else if($("#icon").attr("class","fa-solid fa-eye"))
-    {   $("#pwd").attr("type","password")
-    $("#icon").attr("class","fa-solid fa-eye-slash")
-    }
-   
-  })
-
-  //login validation
-
- 
+  });
 });
